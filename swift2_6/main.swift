090d00a2726998cdb085ca9f21349af61e615537
skyliou520 <pwliu0207@gmail.com>
2016-04-26 20:37:17 +0800

    convert to current swift type

--- swift2_6/main.swift
+++ swift2_6/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-println("Hello, World!")
+class Ticket{
 
+    var _counter:Int = 0{
+        willSet(newValue){
+            print("will change to \(newValue)")
+        }
+        didSet{
+            print("already set to  \(oldValue)")
+        }
+    }
+
+}
+
+let myTicketMachine = Ticket();
+myTicketMachine._counter = 5;
+myTicketMachine._counter = 100;
+myTicketMachine._counter = 200;
